33145d018f5691f8bcc27f76af7335318a916c99
iriverab <iriverab82@gmail.com>
2019-11-21 23:44:22 -0300

    First
    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// FileName: index.js
+// Import express
+let express = require('express')
+// Initialize the app
+let app = express();
+// Setup server port
+var port = process.env.PORT || 8080;
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+// Add the code below to index.js
+// Import routes
+let apiRoutes = require("./api-routes")
+// Use Api routes in the App
+app.use('/api', apiRoutes)
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
